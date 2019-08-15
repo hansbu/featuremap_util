322534233f9f990a4d9efaa34160fcb7be58e813
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-08-15 13:39:21 -0400

    cleanup

--- src/merge_csv.py
+++ src/merge_csv.py
@@ -6,16 +6,14 @@ import pandas as pd
 def wip():
     c1 = str(sys.argv[1])
     c2 = str(sys.argv[2])
+
     df1 = pd.read_csv(c1, delimiter=',')
     df2 = pd.read_csv(c2, delimiter=',')
 
     merged_df = df1.merge(df2, how='left', on=['i', 'j'])
-    # print(merged_df)
+    header = list(merged_df.columns)
     merged_df.fillna(0, inplace=True)
-    # header = ["i", "j", "TIL_y", "Cancer", "Tissue"]
-    # merged_df.to_csv('merged.csv', columns=header)
-    # merged_df.to_csv('merged.csv', columns=header, index=False)
-    merged_df.to_csv('merged.csv')
+    merged_df.to_csv('merged.csv', columns=header, index=False)
 
 
 wip()
