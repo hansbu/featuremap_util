b15d28a8dd02093296339c008ebf0ee75eb0531e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-09-19 15:17:03 -0400

    neatly formatted

--- get_grayscale_heatmaps/get_grayscale_heatmap.sh
+++ get_grayscale_heatmaps/get_grayscale_heatmap.sh
@@ -17,31 +17,31 @@ HEAT_LOC='/data/input'
 
 for files in ${HEAT_LOC}/color-*; do
 
-    # Get slide id
-    SVS=`echo ${files} | awk -F'/' '{print $NF}' | awk -F'color-' '{print $2}'`
+  # Get slide id
+  SVS=$(echo ${files} | awk -F'/' '{print $NF}' | awk -F'color-' '{print $2}')
 
-    # Find the unmodified heatmap
-   PRED=`ls -1 ${HEAT_LOC}/prediction-${SVS}*|grep -v low_res`
-   COLOR=${files}
+  # Find the unmodified heatmap
+  PRED=$(ls -1 ${HEAT_LOC}/prediction-${SVS}* | grep -v low_res)
+  COLOR=${files}
 
-    if [ ! `ls -1 ${SLIDES}/${SVS}*.svs` ]; then
-    	  echo "${SLIDES}/${SVS}.XXXX.svs does not exist. Trying tif..."
-        SVS_FILE=`ls -1 ${SLIDES}/${SVS}*.tif | head -n 1`
-    else
-        SVS_FILE=`ls -1 ${SLIDES}/${SVS}*.svs | head -n 1`
-    fi
+  if [ ! $(ls -1 ${SLIDES}/${SVS}*.svs) ]; then
+    echo "${SLIDES}/${SVS}.XXXX.svs does not exist. Trying tif..."
+    SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.tif | head -n 1)
+  else
+    SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.svs | head -n 1)
+  fi
 
-    if [ -z "$SVS_FILE" ]; then
-        echo "Could not find slide."
-        continue;
-    fi
+  if [ -z "$SVS_FILE" ]; then
+    echo "Could not find slide."
+    continue
+  fi
 
-    WIDTH=` openslide-show-properties ${SVS_FILE} \
-          | grep "openslide.level\[0\].width"  | awk '{print substr($2,2,length($2)-2);}'`
-    HEIGHT=`openslide-show-properties ${SVS_FILE} \
-          | grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}'`
+  WIDTH=$(openslide-show-properties ${SVS_FILE} |
+    grep "openslide.level\[0\].width" | awk '{print substr($2,2,length($2)-2);}')
+  HEIGHT=$(openslide-show-properties ${SVS_FILE} |
+    grep "openslide.level\[0\].height" | awk '{print substr($2,2,length($2)-2);}')
 
-    python get_grayscale_heatmap.py ${SVS} ${WIDTH} ${HEIGHT} ${PRED} ${COLOR}
+  python get_grayscale_heatmap.py ${SVS} ${WIDTH} ${HEIGHT} ${PRED} ${COLOR}
 done
 #cp ./grayscale_heatmaps/* ${GRAYSCALE_HEATMAPS_PATH}/
 
