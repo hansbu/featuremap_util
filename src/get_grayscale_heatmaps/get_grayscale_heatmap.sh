0009faba32a0633c2621c1ac7236fef2abf7322e
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-09-23 14:00:11 -0400

    ext var

--- src/get_grayscale_heatmaps/get_grayscale_heatmap.sh
+++ src/get_grayscale_heatmaps/get_grayscale_heatmap.sh
@@ -6,6 +6,7 @@
 # This is just used for getting the height and width
 # of the slides
 SLIDES='/data/wsi'
+EXT="$1"
 
 # Locations of unmodified heatmaps
 # The filenames of the unmodifed heatmaps should be:
@@ -24,16 +25,10 @@ for files in ${HEAT_LOC}/color-*; do
   PRED=$(ls -1 ${HEAT_LOC}/prediction-${SVS}* | grep -v low_res)
   COLOR=${files}
 
-  if [ ! $(ls -1 ${SLIDES}/${SVS}*.svs) ]; then
-    echo "${SLIDES}/${SVS}.XXXX.svs does not exist. Trying tif..."
-    SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.tif | head -n 1)
-  else
-    SVS_FILE=$(ls -1 ${SLIDES}/${SVS}*.svs | head -n 1)
-  fi
-
-  if [ -z "$SVS_FILE" ]; then
-    echo "Could not find slide."
-    continue
+  SVS_FILE=`ls -1 ${SLIDES}/${SVS}*.$EXT | head -n 1`
+  if [ ! -f ${SVS_FILE} ]; then
+    echo ${SLIDES}/${SVS}.XXXX.$EXT does not exist.
+    continue;
   fi
 
   WIDTH=$(openslide-show-properties ${SVS_FILE} |
