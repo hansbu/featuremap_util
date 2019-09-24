8204d73337297bd73625d158756ef258bb557bd3
Tammy DiPrima <tammy.diprima@stonybrook.edu>
2019-09-24 15:54:17 -0400

    Update get_grayscale_heatmap.sh

--- src/get_grayscale_heatmaps/get_grayscale_heatmap.sh
+++ src/get_grayscale_heatmaps/get_grayscale_heatmap.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-#source ../../conf/variables.sh
+error_exit() {
+   echo "${PROGNAME}: ${1:-"Error"}" 1>&2
+   echo "Line $2"
+   exit 1
+}
 
 # Path contains the svs slides
 # This is just used for getting the height and width
@@ -20,12 +24,15 @@ for files in ${HEAT_LOC}/color-*; do
 
   # Get slide id
   SVS=$(echo ${files} | awk -F'/' '{print $NF}' | awk -F'color-' '{print $2}')
+  if [ ${SVS} == "*" ]; then
+    error_exit "There are no color files." $LINENO
+  fi
 
   # Find the unmodified heatmap
   PRED=$(ls -1 ${HEAT_LOC}/prediction-${SVS}* | grep -v low_res)
   COLOR=${files}
 
-  SVS_FILE=`ls -1 ${SLIDES}/${SVS}*.$EXT | head -n 1`
+  SVS_FILE=`ls -1 ${SLIDES}/${SVS}*.$EXT | head -n 1` || error_exit $LINENO
   if [ ! -f ${SVS_FILE} ]; then
     echo ${SLIDES}/${SVS}.XXXX.$EXT does not exist.
     continue;
